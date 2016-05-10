733b2d1ce233d11db933a9055af5a67c0783d879
narrowdesign <nick@narrowdesign.com>
2016-05-10 15:30:11 -0400

    more components

---
+++ ui/assets/scripts/class-lists.js
@@ -0,0 +1,108 @@
+var Archetypes_array = new Array();
+Archetypes_array[0] = 'Rams';
+Archetypes_array[1] = 'Vignelli';
+Archetypes_array[2] = 'Bass';
+Archetypes_array[3] = 'Brockmann';
+// Archetypes_array[3] = 'Escher';
+
+var section_h_padding = [
+  'section-h-padding-none',
+  'section-h-padding-xxsmall',
+  'section-h-padding-xsmall',
+  'section-h-padding-small',
+  'section-h-padding-medium',
+  'section-h-padding-large',
+  'section-h-padding-xlarge'
+]
+var section_v_padding = [
+  'section-v-padding-none',
+  'section-v-padding-xsmall',
+  'section-v-padding-small',
+  'section-v-padding-medium',
+  'section-v-padding-large',
+  'section-v-padding-xlarge'
+]
+
+var CoverCenter_array = new Array();
+CoverCenter_array['Rams'] = '40% 40%';
+CoverCenter_array['Vignelli'] = '70% 43%';
+CoverCenter_array['Bass'] = '43% 22%';
+CoverCenter_array['Brockmann'] = '52% 29%';
+
+var section_size = [
+  'section-size-fullscreen',
+  'section-size-auto'
+]
+var flex = [
+  'flex-row',
+  'flex-col'
+]
+var title_size = [
+  'title-size-xxsmall',
+  'title-size-xsmall',
+  'title-size-small',
+  'title-size-medium',
+  'title-size-large',
+  'title-size-xlarge',
+  // 'title-size-xxlarge'
+]
+var text_align = [
+  'text-align-left',
+  'text-align-center',
+  'text-align-right',
+  // 'text-align-justify'
+]
+var group_align = [
+  'align-start',
+  'align-center',
+  'align-end',
+  'align-stretch',
+  'align-baseline'
+]
+var group_justify = [
+  'justify-start',
+  'justify-center',
+  'justify-end',
+  'justify-between',
+  'justify-around'
+]
+var is_bg = [
+  'is-bg',
+  'no-bg',
+  'both-bg'
+]
+var bg_fit = [
+  'bg-fit-cover',
+  'bg-fit-contain'
+]
+var bg_color = [
+  'bg-color-light',
+  'bg-color-dark',
+  'bg-color-neutral',
+  'bg-color-pattern',
+  'bg-color-none'
+]
+var bg_size = [
+  'bg-size-full',
+  'bg-size-half',
+  'bg-size-third',
+  'bg-size-quarter'
+]
+var bg_align = [
+  'bg-align-top',
+  'bg-align-center',
+  'bg-align-bottom',
+  'bg-align-stretch'
+]
+var bg_justify = [
+  'bg-justify-left',
+  'bg-justify-center',
+  'bg-justify-right',
+  'bg-justify-stretch'
+]
+var bg_padding = [
+  'bg-padding-xsmall',
+  'bg-padding-small',
+  'bg-padding-medium',
+  'bg-padding-large'
+]
---
+++ ui/assets/scripts/fixtures.js
@@ -0,0 +1,13 @@
+var Headlines_array = new Array();
+Headlines_array['Rams'] = 'Aware that he was a significant contributor to that world, he asked himself an important question: is my design good design?'
+Headlines_array['Vignelli'] = 'Linear Genius, Bold and Transformative ‘Design Is One’ Centers on the Vignelli Team'
+Headlines_array['Bass'] = 'Saul Bass On His Approach To Title Sequences'
+Headlines_array['Brockmann'] = 'I would advise young people to look at everything they encounter in a critical light … Then I would urge them at all times to be self-critical.'
+// Headlines_array['Escher'] = "The ultimate mind-benders: MC Escher's incredible optical illusions are set to have fans questioning reality with his first major UK exhibition"
+
+var Authors_array = new Array();
+Authors_array['Rams'] = 'Dieter Rams via Vitsœ'
+Authors_array['Vignelli'] = 'Lella & Massimo Vignelli via NYTimes'
+Authors_array['Bass'] = 'Saul Bass via Medium'
+Authors_array['Brockmann'] = 'Josef Müller-Brockmann via Coroflot'
+// Authors_array['Escher'] = 'MC Escher via Dailymail'
