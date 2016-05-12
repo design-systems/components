dbbf178f94ee7834e3d50f0bd6ba15f3fb82008e
narrowdesign <nick@narrowdesign.com>
2016-05-12 11:48:09 -0400

    short-titles

--- ui/assets/scripts/fixtures.js
+++ ui/assets/scripts/fixtures.js
@@ -1,10 +1,13 @@
 var Headlines_array = new Array();
-Headlines_array[0] = 'Saul Bass On His Approach To Title Sequences'
-Headlines_array[1] = 'Linear Genius, Bold and Transformative ‘Design Is One’ Centers on the Vignelli Team'
-Headlines_array[2] = 'Aware that he was a significant contributor to that world, he asked himself an important question: is my design good design?'
-Headlines_array[3] = 'I would advise young people to look at everything they encounter in a critical light … Then I would urge them at all times to be self-critical.'
-Headlines_array[4] = 'Business owners need to realize that their design is a reflection of their business even if it is not intentional. If you don’t care about your design then your design is telling people that you don’t care about your business.'
-Headlines_array[5] = 'I want everything we do to be beautiful. I don’t give a damn whether the client understands that that’s worth anything, or that the client thinks it’s worth anything, or whether it is worth anything. It’s worth it to me. It’s the way I want to live my life. I want to make beautiful things, even if nobody cares.'
+Headlines_array[0] = 'So What?'
+Headlines_array[0] = 'Don’t make me think.'
+Headlines_array[1] = 'Saul Bass On His Approach To Title Sequences'
+Headlines_array[2] = 'I fill up a place, which may be better… when I have made it empty.'
+Headlines_array[3] = 'Linear Genius, Bold and Transformative ‘Design Is One’ Centers on the Vignelli Team'
+Headlines_array[4] = 'Aware that he was a significant contributor to that world, he asked himself an important question: is my design good design?'
+Headlines_array[5] = 'I would advise young people to look at everything they encounter in a critical light … Then I would urge them at all times to be self-critical.'
+Headlines_array[6] = 'Business owners need to realize that their design is a reflection of their business even if it is not intentional. If you don’t care about your design then your design is telling people that you don’t care about your business.'
+Headlines_array[7] = 'I want everything we do to be beautiful. I don’t give a damn whether the client understands that that’s worth anything, or that the client thinks it’s worth anything, or whether it is worth anything. It’s worth it to me. It’s the way I want to live my life. I want to make beautiful things, even if nobody cares.'
 
 var Authors_array = new Array();
 Authors_array['Rams'] = 'Dieter Rams via Vitsœ'
